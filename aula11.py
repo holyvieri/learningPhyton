af1e2fe4d991043ed59e7a8530e22852d13bfa2b
Ana Oliveira <beatriz.oliveirac@upe.br>
2023-06-10 18:26:18 -0300

    Mundo Phyton 1
    cores no terminal - aula

---
+++ aula11.py
@@ -0,0 +1,5 @@
+print('\033[1;31;40mOlá, Mundo!')
+print('\033[1;30;41mOlá, Mundo!\033[m')
+print('\033[mOlá, Mundo!')
+print('\033[0;33;44mOlá, Mundo!\033[m')
+print('\033[7;33;44mOlá, Mundo!\033[m')
